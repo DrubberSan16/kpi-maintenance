f4ca47d4a13a2e37fef115ce36c5c2814b007ad9
DrubberSan16 <drubber2000@hotmail.com>
2026-02-28 17:59:32 +0000

    Changue schema

--- src/modules/entities/index.ts
+++ src/modules/entities/index.ts
@@ -1,16 +1,5 @@
-import { Bodega } from "./bodega.entity";
-import { Categoria } from "./categoria.entity";
-import { Kardex } from "./kardex.entity";
-import { Linea } from "./linea.entity";
-import { Marca } from "./marca.entity";
-import { MovimientoInventario } from "./movimiento-inventario.entity";
-import { MovimientoInventarioDet } from "./movimiento-inventario-det.entity";
-import { Producto } from "./producto.entity";
-import { StockBodega } from "./stock-bodega.entity";
-import { Sucursal } from "./sucursal.entity";
-import { Tercero } from "./tercero.entity";
-import { UnidadMedida } from "./unidad-medida.entity";
 
-export const ENTITIES = [Bodega, Categoria, Kardex, Linea, Marca, MovimientoInventario, MovimientoInventarioDet, Producto, StockBodega, Sucursal, Tercero, UnidadMedida];
 
-export { Bodega, Categoria, Kardex, Linea, Marca, MovimientoInventario, MovimientoInventarioDet, Producto, StockBodega, Sucursal, Tercero, UnidadMedida };
+export const ENTITIES = [];
+
+export { };
